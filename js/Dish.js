ab006d4674ea6caf34d66c2c005cbee46fa05f01
Adrián <hyron529@gmail.com>
2024-01-10 12:50:39 +0100

    primer commit

---
+++ js/Dish.js
@@ -0,0 +1,32 @@
+
+/**
+ * Creación de la clase Dish, que nos va a permitir almacenar
+ * todos los objetos de tipo plato deseados
+ */
+class Dish{
+
+    //Primero definimos las propiedades que va a tener Dish
+
+    #name;
+    #description;
+    #ingredients;
+    #image;
+
+    /*
+        Definición del constrructor de la clase Dish
+        Pasando el nombre inicialmente, vamos a comprobar si no se introduce un nombre de plato,
+        o si el operador new no ha sido utilizado, por lo que lanzamos excepción
+        Una vez comprobado, podemos definir nuestras variables
+    */
+    constructor(name){
+
+        if (name == undefined || name == "") throw new EmptyValueException("name");
+
+        if (!(new.target == Dish)) throw new InvalidConstructorException("Dish");
+
+        this.#name = name;
+        this.#description = null;
+        this.#ingredients = null;
+        this.#image = null;
+    }
+}
